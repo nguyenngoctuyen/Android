c56ca7510692e51b6c6766afdc129af96fcd7c0f
nguyenngoctuyen <rynguyen95@gmail.com>
2017-05-10 23:45:28 +0700

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.admin.myapplication;
+package com.example.admin.vd_bmi;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,73 +9,49 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txtkq;
-    EditText ea, eb;
-    Button btntong, btntru, btnnhan, btnchia;
-
+    TextView txtkqbmi, txtkqcd;
+    Button btntinh;
+    EditText edcc, edcn;
+    String chandoan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtkq = (TextView) findViewById(R.id.kq);
-        ea = (EditText) findViewById(R.id.eda);
-        eb = (EditText) findViewById(R.id.edb);
-        btntong = (Button) findViewById(R.id.tong);
-        btntru = (Button) findViewById(R.id.tru);
-        btnnhan = (Button) findViewById(R.id.nhan);
-        btnchia = (Button) findViewById(R.id.chia);
-        Kiemtra(ea);
-        Kiemtra(eb);
-        btntong.setOnClickListener(new View.OnClickListener() {
+        txtkqbmi = (TextView) findViewById(R.id.kqBMI);
+        txtkqcd = (TextView) findViewById(R.id.kqcd);
+        btntinh = (Button) findViewById(R.id.tinh);
+        edcc = (EditText) findViewById(R.id.cao);
+        edcn = (EditText) findViewById(R.id.can);
+        btntinh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (Kiemtra(ea) == false || Kiemtra(eb) == false)
-                    txtkq.setText("Moi nhap so");
+                double H=Double.parseDouble(edcc.getText()+"");
+                double W=Double.parseDouble(edcn.getText()+"");
+                double BMI=W/Math.pow(H, 2);
+                if(BMI<18)
+                {
+                    chandoan="Bạn gầy";
+                }
+                else if(BMI<=24.9)
+                {
+                    chandoan="Bạn bình thường";
+                }
+                else if(BMI<=29.9)
+                {
+                    chandoan="Bạn béo phì độ 1";
+                }
+                else if(BMI<=34.9)
+                {
+                    chandoan="Bạn béo phì độ 2";
+                }
                 else
-                    txtkq.setText(Integer.parseInt(ea.getText() + "") + Integer.parseInt(eb.getText() + "") + "");
+                {
+                    chandoan="Bạn béo phì độ 3";
+                }
+                txtkqcd.setText(chandoan);
+                txtkqbmi.setText(BMI + "");
 
-            }
-        });
-        btntru.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (Kiemtra(ea) == false || Kiemtra(eb) == false)
-                    txtkq.setText("Moi nhap so");
-                else
-                    txtkq.setText(Integer.parseInt(ea.getText() + "") - Integer.parseInt(eb.getText() + "") + "");
-            }
-        });
-        btnnhan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (Kiemtra(ea) == false || Kiemtra(eb) == false)
-                    txtkq.setText("Moi nhap so");
-                else
-                    txtkq.setText(Integer.parseInt(ea.getText() + "") * Integer.parseInt(eb.getText() + "") + "");
-            }
-        });
-        btnchia.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (Kiemtra(ea) == false || Kiemtra(eb) == false)
-                    txtkq.setText("Moi nhap so");
-                else
-                    txtkq.setText((float) Integer.parseInt(ea.getText() + "") / Integer.parseInt(eb.getText() + "") + "");
             }
         });
     }
-
-    public boolean Kiemtra(EditText n) {
-        try {
-            Integer.parseInt(n.getText() + "");
-        } catch (Exception e) {
-            return false;
-        }
-        return true;
-
-    }
-
 }
-
-
-
